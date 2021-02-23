2e10c794653b6c6518c92da289ff590d83aa6215
Khasbullah Nukman Zakin <nukmans23@gmail.com>
2021-02-24 00:35:16 +0700

    updating

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
-import Control from "./components/Control/Control";
+import Title from "./components/Title/Title";
+import Progress from "./components/Progress/Progress";
+import Controller from "./components/Control/Control";
 function App() {
   return (
     <div className="container">
       <Header />
       <Main />
-      <Control />
+      <Title />
+      <Progress />
+      <Controller />
     </div>
   );
 }
