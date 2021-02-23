c6a6f2af11ceca19720fae7aef11959259768f84
Khasbullah Nukman Zakin <nukmans23@gmail.com>
2021-02-23 16:12:21 +0700

    adding

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div>
+      <img
+        className="album"
+        src="https://i.scdn.co/image/ab67616d0000b2738d7d3584e5220d0cd406eea4"
+        alt="album"
+      />
+    </div>
+  );
+};
+
+export default Main;
