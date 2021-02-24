825dbb8e95a5fffaee4ed4350e3d9b7050e38b7c
Khasbullah Nukman Zakin <nukmans23@gmail.com>
2021-02-24 11:38:32 +0700

    react standard update

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -3,12 +3,12 @@ import "./Progress.css";
 
 const Progress = () => {
   return (
-    <div class="song-progress-container">
-      <p class="timer-start">0:00</p>
-      <div class="song-progress">
-        <div class="song-expired"></div>
+    <div className="song-progress-container">
+      <p className="timer-start">0:00</p>
+      <div className="song-progress">
+        <div className="song-expired"></div>
       </div>
-      <p class="timer-end">3:17</p>
+      <p className="timer-end">3:17</p>
     </div>
   );
 };
