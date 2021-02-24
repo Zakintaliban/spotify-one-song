269c540c1aa6b8403a04078b9109ab1e664043e9
Khasbullah Nukman Zakin <nukmans23@gmail.com>
2021-02-24 10:55:22 +0700

    done, for mobile version

--- src/components/Control/Control.js
+++ src/components/Control/Control.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Control.css";
-import { BsShuffle } from "react-icons/bs";
+import { BsShuffle, BsFillVolumeUpFill } from "react-icons/bs";
 import { FiRepeat } from "react-icons/fi";
+import { FaRegPlayCircle } from "react-icons/fa";
+import { GiNextButton, GiPreviousButton } from "react-icons/gi";
 const Control = () => {
   return (
     <div className="controller">
@@ -13,8 +15,20 @@ const Control = () => {
           <FiRepeat className="repeat__svg" color="white" />
         </div>
       </div>
-      <div className="control"></div>
-      <div className="volume"></div>
+      <div className="control">
+        <div className="prev">
+          <GiPreviousButton className="prev__svg" color="white" />
+        </div>
+        <div className="play">
+          <FaRegPlayCircle className="play__svg" color="white" />
+        </div>
+        <div className="next">
+          <GiNextButton className="next__svg" color="white" />
+        </div>
+      </div>
+      <div className="vol">
+        <BsFillVolumeUpFill className="vol__svg" color="white" />
+      </div>
     </div>
   );
 };
